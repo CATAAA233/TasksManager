718171105f8af2abd98b6809b107be929763bd3a
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2023-02-24 18:39:57 -0800

    MDI configuration

--- app.vue
+++ app.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
-    <v-btn>
+    <v-btn color="black">
       Hello world!
+
+      <v-icon icon="md:info"></v-icon>
     </v-btn>
+
+    <v-btn
+        class="ma-2"
+        color="primary"
+      >
+        Accept
+        <v-icon
+          end
+          icon="mdi-checkbox-marked-circle"
+        ></v-icon>
+      </v-btn>
   </div>
 </template>
