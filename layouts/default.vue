15fc4d36fe034de88d46f8913d45066f0c6f8d41
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2023-02-27 00:19:27 -0800

    fixing overflow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,50 +1,56 @@
 <template>
-    <div class="Page">
-        <SideBar />
-        <div class="Page__Body">
-            <slot />
-        </div>
-        <div class="ProfileStatics__Container Container__Hidden">
-            <ProfileStatics />
-        </div>
+  <div class="Page">
+    <SideBar />
+    <div class="Page__Body">
+      <slot />
     </div>
+    <div class="ProfileStatics__Container Container__Hidden">
+      <ProfileStatics />
+    </div>
+  </div>
 </template>
 
 <style>
-  body{
-    background-color: #f1f1f1;
-    height: 100vh;
-    padding: 3.5rem;
-  }
-  .page-enter-active,
-  .page-leave-active {
-    transition: all 0.4s;
-  }
+body {
+  background-color: #f1f1f1;
+  height: 100vh;
+  padding: 3.5rem;
+  overflow: hidden;
+}
 
-  .page-enter-from,
-  .page-leave-to {
-    opacity: 0;
-    filter: blur(1rem);
-  }
-    .Page {
-        display: flex;
-        height: calc(100vh - 7rem);
-        column-gap: 3rem;
-    }
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
 
-    .Page__Body {
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-    row-gap: 4rem;
-    overflow-y: scroll;
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+
+.Page {
+  display: flex;
+  height: calc(100vh - 7rem);
+  column-gap: 3rem;
+}
+
+.Page__Body {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  row-gap: 4rem;
+  overflow-y: scroll;
 }
 
 @media screen and (max-height: 800px) {
-    .Page__Body{
-        height: 43rem;
-    }
-    
-}
+  body {
+    overflow-y: scroll;
+  }
 
+  .Page__Body {
+    height: 43rem;
+  }
+
+}
 </style>
